8297c6a0027f42203ec00b213fcad7ac8e98ba6f
bkalish <bkalish@gmail.com>
2015-06-08 20:46:14 -0400

    begun migration code. forbes_database_categories now added for existing posts

--- forbes-databases.php
+++ forbes-databases.php
@@ -57,6 +57,60 @@ function forbes_databases_flush_rewrites() {
 function forbes_databases_init() {
   forbes_databases_register_custom_post_types();
   forbes_databases_register_custom_taxonomies();
+  forbes_databases_migrate();
+}
+
+/**
+ * Migrate data from earlier versions
+ */
+function forbes_databases_migrate() {
+  global $post;
+  $version = get_option('forbes_databases_version');
+
+  // Migrate to version 1.0.0. Earlier versions had hardcoded access rules
+  if (!$version) {
+    $query = new WP_Query(
+      array(
+        'post_type' => 'forbes_databases',
+        'nopaging' => true
+      )
+    );
+    if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) {
+        $query->the_post();
+        $custom = get_post_custom($post->ID);
+        $availability = $custom["database_availability"][0];
+        if (!term_exists($availability, 'forbes_database_categories')) {
+          switch ($availability) {
+            case 'state-wide':
+              $name = "Free State Wide.";
+              break;
+            case 'cwmars':
+              $name = "Free With C/W Mars Card";
+              break;
+            case 'forbes-card':
+              $name = "Free With Forbes Card";;
+              break;
+            case 'bpl-ecard':
+              $name = 'Free With BPL ECard';
+              break;
+            case 'in-library':
+              $name = 'Free In Library';
+              break;
+            case 'anywhere':
+              $name = 'Free Anywhere';
+          }
+          $args = array(
+            'description' => forbes_databases_get_availability_text($post),
+            'slug' => $availability
+          );
+          wp_insert_term($name, 'forbes_database_categories', $args);
+        }
+        wp_set_object_terms($post->ID, $availability, 'forbes_database_categories');
+      }
+    }
+    wp_reset_query();
+  }
 }
 
 /**
