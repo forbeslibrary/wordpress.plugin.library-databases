874fdb91c5e9b8f05a73f5ad03107781225e134b
bkalish <bkalish@gmail.com>
2015-06-17 14:35:35 -0400

    media chooser javascript now working and image id saved in database

--- categories-admin.php
+++ categories-admin.php
@@ -38,6 +38,12 @@ class Library_Databases_Categories_Admin {
         $term_meta['library_use_only'] = false;
       }
 
+      if (is_numeric($_POST['term_meta']['image'])) {
+        $term_meta['image'] = intval($_POST['term_meta']['image']);
+      } else {
+        $term_meta['image'] = null;
+      }
+
       // Save the option array.
       update_option( "taxonomy_{$term_id}", $term_meta );
 
@@ -63,12 +69,12 @@ class Library_Databases_Categories_Admin {
   function add_form_fields(){
     ?>
     <div class="form-field">
-      <label>
+      <label for="lib_databases_media_button">
         <div class="label">
           <?php _e('Image'); ?>
         </div>
-        <input type="file" name="term_meta[image]"/>
       </label>
+      <?php echo $this->image_form_fields(); ?>
     </div>
     <div class="form-field">
       <div class="label">
@@ -90,24 +96,15 @@ class Library_Databases_Categories_Admin {
    */
   function edit_form_fields($term) {
     $term_meta = get_option( "taxonomy_{$term->term_id}" );
-    var_dump($term_meta);
     ?>
     <tr class="form-field">
       <th scope="row">
-        <label for="image_upload_button">
+        <label for="image_choose_button">
           <?php _e('Image'); ?>
         </label>
       </th>
       <td>
-        <input type="hidden"
-            class="metaValueField"
-            id="term_meta[image]"
-            name="term_meta[image]"
-            value="<?php echo $term_meta['image']; ?>"
-          />
-          <div id="metaImage"></div>
-          <input class="image_upload_button"  type="button" value="Choose File" />
-          <input class="removeImageBtn" type="button" value="Remove File" />
+        <?php echo $this->image_form_fields($term); ?>
       </td>
     </tr>
     <tr class="form-field">
@@ -127,6 +124,33 @@ class Library_Databases_Categories_Admin {
     <?php
   }
 
+  function image_form_fields($term = null) {
+    if ($term) {
+      $term_meta = get_option( "taxonomy_{$term->term_id}" );
+      $has_image = isset($term_meta['image']);
+    } else {
+      $term_meta = array();
+      $has_image = false;
+    }
+    ob_start();
+    ?>
+    <input type="hidden"
+      class="metaValueField"
+      id="term_meta[image]"
+      name="term_meta[image]"
+      value="<?php if ($has_image) { echo $term_meta['image']; } ?>"
+    />
+    <div id="lib_databases_thumbnail">
+      <?php if ($has_image): ?>
+        <?php echo wp_get_attachment_image( $term_meta['image']); ?>
+      <?php endif; ?>
+    </div>
+    <input id="lib_databases_media_button" class="image_choose_button"  type="button" value="Choose File" />
+    <input class="removeImageBtn" type="button" value="Remove File" style="display:none;" />
+    <?php
+    return ob_get_clean();
+  }
+
   /**
    * Add JavaScript to get URL from media uploader.
    */
@@ -136,29 +160,56 @@ class Library_Databases_Categories_Admin {
         or $screen->taxonomy != 'lib_databases_categories') {
       return;
     }
+    wp_enqueue_media();
     ?>
     <script type="text/javascript">
     jQuery(document).ready(function() {
 
-      jQuery('.removeImageBtn').click(function() {
-        jQuery('#awdMetaImage').html('');
-        jQuery('#term_meta[image]').val('');
+      var hidden_field = jQuery('#term_meta\\[image\\]');
+      var remove_button = jQuery('.removeImageBtn');
+      var choose_button = jQuery('.image_choose_button');
+      var image_wrapper = jQuery('#lib_databases_thumbnail');
+
+      if (hidden_field.val()) {
+        remove_button.show();
+      }
+
+      remove_button.click(function() {
+        image_wrapper.html('');
+        hidden_field.val('');
+        remove_button.hide();
         return false;
       });
 
-      jQuery('.image_upload_button').click(function() {
-        inputField = jQuery(this).prev('.metaValueField');
-        tb_show('', 'media-upload.php?TB_iframe=true');
-        window.send_to_editor = function(html) {
-          url = jQuery(html).attr('href');
-          inputField.val(url);
-          jQuery('#awdMetaImage').html('<p>URL: '+ url + '</p>');
-          tb_remove();
-        };
-        return false;
+      choose_button.click(function() {
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+          title: 'Choose Image',
+          library: { type: 'image' },
+          button: { text: 'Choose Image' },
+          multiple: false
+        });
+
+        // set selected image on open
+        custom_uploader.on('open', function() {
+          var selection = custom_uploader.state().get('selection');
+          var id = hidden_field.val();
+          var attachment = wp.media.attachment(id);
+          attachment.fetch();
+          selection.add( attachment ? [ attachment ] : [] );
+        });
+
+        // get selected image on select
+        custom_uploader.on('select', function() {
+          var attachment = custom_uploader.state().get('selection').first().toJSON();
+          hidden_field.val(attachment.id);
+          var thumb = jQuery('<img>').attr('src', attachment.sizes.thumbnail.url);
+          image_wrapper.html(thumb);
+          remove_button.show();
+        });
+
+        custom_uploader.open();
       });
     });
-
     </script>
     <?php
   }
