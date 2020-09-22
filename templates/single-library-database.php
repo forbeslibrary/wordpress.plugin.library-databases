4396fef4167238311aeb712cde6632c4cb80abd8
Benjamin Kalish <bkalish@gmail.com>
2020-09-22 15:59:50 -0400

    added breadcrumb to single database pages

--- templates/single-library-database.php
+++ templates/single-library-database.php
@@ -3,9 +3,15 @@
 Template Name: Single Library Database
 */
 $post = get_post();
+$archive_link = get_post_type_archive_link( $post->post_type );
+$name = get_post_type()->labels->name;
 
 get_header();
 ?>
+<div class="crumbs breadcrumbs"><a href="<?php echo $archive_link; ?>:>
+<?php echo $name ?></a>→<?php echo $post->title; ?>
+</div>
+<?php echo </div>
 <div id="content">
 <?php load_template( dirname( __FILE__ ) . '/partials/content-library-database.php', False ); ?>
 </div>
